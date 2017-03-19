c8a4ee56f34074305a4362eb62cf3f5caa8eb952
Catupper <nekarugo628@gmail.com>
2017-03-19 23:36:55 +0900

    make progress

--- application/modules/default/controllers/SearchController.php
+++ application/modules/default/controllers/SearchController.php
@@ -13,8 +13,57 @@ require_once '../application/modules/default/models/MainModel.php';
 class SearchController extends Zend_Controller_Action
 {
 
+    public function getDist($a, $b){
+        //get the length of the shortest from $a to $b
+        return $a+$b;//just a dummy
+    }
+
+    public function getStartTime($id){
+        //get the start time of the event $id
+        return 0;
+    }
+
+    
+    public function getEndTime($id){
+        //get the start time of the event $id
+        return 0;
+    }
+    
     public function indexAction(){
-        $this->_request->getPost('');
+        $req = $this->getRequest();
+        $params = $req->getParms();
+        $beginTime = $params["clock1"];
+        $endTime = $params["clock2"];
+        $param_keys = array_keys($parms);
+        $checkpoints = array();
+        foreach ($keys as $key){
+            if(substr($key, 0, 5) != "input")continue;
+            if($parms[$key] == "")continue;
+            $checkpoints += array(intval(substr($key, 5, -1)));
+        }
+      
+        $n = count($checkpoints);
+
+        $startPos = $parms["startPos"];
+
+        
+        $inputData = "";
+        $inputData .= sprintf("%d %d\n", $n, $startPos);
+        for($i = 0; $i < $n; $i++){
+            $eventID = $checkpoints[$i];
+            $startTime = getStartTime($eventID);
+            $endTime = getEndTime($eventID);
+            $inputData .= sprintf("%d %d %d\n", $eventID, $startTime, $endTime);   
+        }
+        for($i = 0;$i < count($checkpoitns); $i++){
+            for($j = -1;$j < $n; $j++){
+                $from = $i;
+                $to = $j;
+                if($to == -1)$to = $startPos;
+                $inputData .= sprintf("%d ", getDist($from, $to));
+            }
+        }
+        
         
         $inout = array(
             0 => array('pipe', 'r'),
@@ -22,19 +71,22 @@ class SearchController extends Zend_Controller_Action
             2 => array('pipe', 'w')
         );
 
+        
         $proc = proc_open('/var/www/scripts/search.out', $inout, $pipes);
         
         if(is_resource($proc)){
-            fwrite($pipes[0],"just_echo");
+            fwrite($pipes[0], $inputData);
             fclose($pipes[0]);
 
-            echo stream_get_contents($pipes[1]);
+            $answer = array_map(intval, explode($pipes[1], "\n"));  
+            
+
+            //setparams ... not yet
+            
             fclose($pipes[1]);
             fclose($pipes[2]);
 
         }
         $this->view->result = "test";
-
-    
     }
 }
