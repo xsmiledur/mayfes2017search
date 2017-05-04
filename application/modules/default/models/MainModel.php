598b5d9439a98754b7b8e1e5401d7426b56fc219
Yuri Mikawa <xsmiledur.8x7@gmail.com>
2017-05-04 19:51:22 +0900

    aa

--- application/modules/default/models/MainModel.php
+++ application/modules/default/models/MainModel.php
@@ -39,6 +39,7 @@ class MainModel
             'driver_options' => $pdoParams
         );
 
+        exit();
         // データベースアダプタを作成する
         $this->_read = Zend_Db::factory($db_read['type'], $read_params);
         // 文字コードをUTF-8に設定する
