fdcb851f1029d1116d74e1ace4658059a78e3e5c
Yuri Mikawa <xsmiledur.8x7@gmail.com>
2017-05-13 22:33:22 +0900

    実験

--- application/modules/default/controllers/SearchController.php
+++ application/modules/default/controllers/SearchController.php
@@ -426,7 +426,6 @@ class SearchController extends Zend_Controller_Action
     }
 
     private function returnResult($proc, $pipes, $inputData, $research, $N, $clock1, $clock2, $date, $start_pos, $time) {
-        var_dump(is_resource($proc));
         if(is_resource($proc)){
             $connect = $this->connectCproject($pipes, $inputData);
             if (substr($connect,0,2) == "-1") {
@@ -434,6 +433,7 @@ class SearchController extends Zend_Controller_Action
                 return 0;
             } else {
                 $pt_pid = array_map('intval', explode("\n", $connect)); //explodeは文字列を文字列で分解する関数
+                var_dump($pt_pid);
                 unset($pt_pid[$N + 1]);
 
                 $this->sendSession($clock1, $clock2, $date, $start_pos, $pt_pid, $time);
