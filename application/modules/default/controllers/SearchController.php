b09aa014665e65e5cdc462ea11741c0712101332
Yuri Mikawa <xsmiledur.8x7@gmail.com>
2017-05-13 23:07:07 +0900

    a

--- application/modules/default/controllers/SearchController.php
+++ application/modules/default/controllers/SearchController.php
@@ -430,6 +430,7 @@ class SearchController extends Zend_Controller_Action
     private function returnResult($proc, $pipes, $flg, $inputData, $research, $N, $clock1, $clock2, $date, $start_pos, $time) {
         if(is_resource($proc)){
             $connect = $this->connectCproject($pipes, $inputData);
+            var_dump($connect)
             if (substr($connect,0,2) == "-1") {
                 if ($research) $this->_session->errMsg = "設定した時間では最適な結果がありませんでした。";
                 return 0;
@@ -446,7 +447,7 @@ class SearchController extends Zend_Controller_Action
                 return 1;
             }
         } else {
-            return 0;
+            return -1;
         }
     }
 
